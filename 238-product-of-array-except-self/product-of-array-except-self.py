15b196ccd2eb83cc8fc8082311cdd4a93e703aac
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-04-17 17:30:51 -0700

    LC238

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = [1] * (len(nums))
+        n = len(nums)
+        res = [1] * (n)
 
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             res[i] = res[i-1] * nums[i-1]
         postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(n - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
         return res

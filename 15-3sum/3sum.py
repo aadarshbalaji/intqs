b47c9faf50fedd0dba48caf7bf31add646de81c8
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-05-14 09:27:57 -0700

    Time: 861 ms (64.33%) | Memory: 15.2 MB (37.53%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -7,16 +7,19 @@ class Solution(object):
         nums = sorted(nums)
         res = []
         for i in range(0, len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             left = i + 1
             right = len(nums) - 1
             while left < right:
                 #print(left, right)
-                if nums[left] + nums[right] == -nums[i]:
-                    if [nums[i], nums[left], nums[right]] not in res:
-                        res.append([nums[i], nums[left], nums[right]])
+                total = nums[left] + nums[right] + nums[i]
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
                     left += 1
-                    right -= 1
-                elif nums[left] + nums[right] < -nums[i]:
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+                elif total < 0:
                     left += 1
                 else:
                     right -= 1

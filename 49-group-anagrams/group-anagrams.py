77aef2bbd41b9b82cf653ae18cd2421fa5d4841e
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-06-25 16:49:24 -0400

    Time: 92 ms (47.11%) | Memory: 22.3 MB (11.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,9 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        maps = []
-        rvlist = []
-        for word in strs:
-            h = {letter:word.count(letter) for letter in word}
-            if h in maps:
-                rvlist[maps.index(h)].append(word)
-            else:
-                maps.append(h)
-                rvlist.append([word])
-        return rvlist
-
-
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()

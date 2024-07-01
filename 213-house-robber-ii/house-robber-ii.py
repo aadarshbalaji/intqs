638f6c8edfe0b9888cf5be608aea45711bb0e5fb
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-07-01 15:39:41 -0400

    Time: 39 ms (36.84%) | Memory: 16.3 MB (90.81%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob1(smol):
+            print(smol)
+            r1,r2 = 0,0
+            for num in smol:
+                temp = max(num + r1, r2)
+                r1 = r2
+                r2 = temp
+            return r2
+        return max(nums[0] + rob1(nums[2:len(nums)-1]), rob1(nums[1:]))

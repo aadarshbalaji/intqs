c7e1506dadd9f77464c584ae54827f3e011d66a5
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-07-03 13:20:54 -0400

    Time: 21 ms (99.63%) | Memory: 16.4 MB (51.67%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
@@ -2,8 +2,6 @@ class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
         if n == 0:
             return 1
-        if n == 1:
-            return 10
         past = self.countNumbersWithUniqueDigits(n-1)
         tot = 9
 

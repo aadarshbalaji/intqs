c5bcc6f683aa430375f087819b1f375831c7f967
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-21 19:59:05 -0700

    Time: 280 ms (49.83%) | Memory: 63.9 MB (18.07%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,43 +1,33 @@
-class RandomizedSet(object):
+class RandomizedSet:
 
     def __init__(self):
         self.hs = {}
         self.arr = []
 
-    def insert(self, val):
-        """
-        :type val: int
-        :rtype: bool
-        """
+    def insert(self, val: int) -> bool:
         if val in self.hs:
             return False
-        else:
-            self.hs[val] = len(self.arr) 
-            self.arr.append(val)
-            return True
+        self.hs[val] = len(self.arr)
+        self.arr.append(val)
+        return True
 
-    def remove(self, val):
-        """
-        :type val: int
-        :rtype: bool
-        """
-        if val in self.hs:
-            last = self.arr[-1]
-            index = self.hs[val]
-            self.arr[index] = last
-            self.hs[last] = index
-            self.arr[-1] = val
-            self.arr.pop()
-            self.hs.pop(val)
-            return True
-        else:
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hs:
             return False
+        last = self.arr[-1]
+        index = self.hs[val]
+        self.arr[index] = last
+        self.hs[last] = index
+        self.arr[-1] = val
+        self.arr.pop()
+        self.hs.pop(val)
+        return True
+        
 
-    def getRandom(self):
-        """
-        :rtype: int
-        """
-        ind = random.randint(0, len(self.arr)-1)
+    def getRandom(self) -> int:
+        #print(self.arr)
+        ind = random.randint(0,len(self.arr)-1)
         return self.arr[ind]
 
 

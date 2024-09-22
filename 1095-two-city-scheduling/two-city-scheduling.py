a5aadad1d1e531656dfa3984489ece590b4874d2
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-22 00:30:12 -0700

    Time: 39 ms (5.80%) | Memory: 11.6 MB (88.41%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,23 +1,29 @@
-class Solution:
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        distances = [[x[0]-x[1], i] for i,x in enumerate(costs)]
-        distances.sort(key=lambda x: abs(x[0]), reverse=True)
-        ca = 0
-        cb = 0
+class Solution(object):
+    def twoCitySchedCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        greedy = [[x[0]-x[1], i] for i,x in enumerate(costs)]
+        counta = 0
+        countb = 0
+        greedy.sort(key=lambda x: abs(x[0]), reverse=True)
+        n = len(costs) // 2
+        i = 0
         total = 0
-        n = len(costs)//2
-        while ca < n and cb < n:
-            diff, index = distances.pop(0)
+        while counta < n and countb < n:
+            diff, index = greedy[i]
             if diff > 0:
-                cb += 1
                 total += costs[index][1]
+                countb += 1
             else:
-                ca += 1
                 total += costs[index][0]
-        if ca >= n:
-            for v, index in distances:
+                counta += 1
+            i += 1
+        if counta >= n:
+            for d, index in greedy[i:]:
                 total += costs[index][1]
-        else:
-            for v, index in distances:
+        if countb >= n:
+            for d, index in greedy[i:]:
                 total += costs[index][0]
         return total

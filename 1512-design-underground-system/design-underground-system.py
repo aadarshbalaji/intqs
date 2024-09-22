4083c8ba3e20ac25227b7c1d330dc48a2090f8fc
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-22 00:05:24 -0700

    Time: 204 ms (66.26%) | Memory: 26.6 MB (61.80%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,44 +1,20 @@
-class UndergroundSystem(object):
+class UndergroundSystem:
 
     def __init__(self):
         self.checkin = {}
-        self.line = (defaultdict(lambda: [0,0]))
+        self.line = defaultdict(lambda: [0,0])
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.checkin[id] = [stationName, t]
 
-    def checkIn(self, id, stationName, t):
-        """
-        :type id: int
-        :type stationName: str
-        :type t: int
-        :rtype: None
-        """
-        self.checkin[id] = (stationName, t)
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        loc, starttime = self.checkin[id]
+        x,y = self.line[(loc, stationName)] 
+        self.line[(loc, stationName)] = [x+t-starttime, y+1]
+        self.checkin.pop(id)
 
-        
-
-    def checkOut(self, id, stationName, t):
-        """
-        :type id: int
-        :type stationName: str
-        :type t: int
-        :rtype: None
-        """
-        loc, time = self.checkin.pop(id)
-        total_time, count = self.line[(loc, stationName)]
-        self.line[(loc, stationName)] = [total_time + t-time, count + 1]
-       
-        
-
-    def getAverageTime(self, startStation, endStation):
-        """
-        :type startStation: str
-        :type endStation: str
-        :rtype: float
-        """
-        tup = (startStation, endStation)
-        time, count = self.line[tup]
-        #print(float(time)/float(count))
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        time,count = self.line[(startStation, endStation)]
         return float(time)/float(count)
-        
 
 
 # Your UndergroundSystem object will be instantiated and called as such:

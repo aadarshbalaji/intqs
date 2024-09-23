3fc9f9667ad991ce5083432bbd39814ce791f8b6
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-22 20:30:28 -0700

    Time: 33 ms (81.74%) | Memory: 17.2 MB (59.06%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -1,6 +1,6 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val, prev, next, child):
         self.val = val
         self.prev = prev
@@ -8,34 +8,28 @@ class Node(object):
         self.child = child
 """
 
-class Solution(object):
-    def flatten(self, head):
-        """
-        :type head: Node
-        :rtype: Node
-        """
-        if head == None:
-            print('h')
-            return head
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         start = head
+        if not head:
+            return head
         while head.next:
             if head.child:
                 flattenchild = self.flatten(head.child)
                 end = flattenchild
                 while end.next:
                     end = end.next
-                end.next = head.next
                 head.next.prev = end
+                end.next = head.next
                 head.next = flattenchild
                 flattenchild.prev = head
                 head.child = None
-                head = end
-            head = head.next
+            else:
+                head = head.next
         while head.child:
             flattenchild = self.flatten(head.child)
             head.next = flattenchild
             flattenchild.prev = head
             head.child = None
             head = head.next
-            
         return start

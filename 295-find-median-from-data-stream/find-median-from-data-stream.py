dbbd1a1158f1f716e2e01c2acc3b4e2d5cb2cf55
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-24 12:45:30 -0700

    Time: 697 ms (42.02%) | Memory: 36 MB (84.36%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,32 +1,42 @@
-class MedianFinder:
+class MedianFinder(object):
 
     def __init__(self):
         self.smallernumbers = [] #negative insertion
-        self.largernumbers = [] 
+        self.largernumbers = []
 
-    def addNum(self, num: int) -> None:
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
         heapq.heappush(self.smallernumbers, -num)
-        if self.smallernumbers and self.largernumbers and -self.smallernumbers[0] > self.largernumbers[0]:
-            heapq.heappush(self.largernumbers, -heapq.heappop(self.smallernumbers))
-           
+        if self.largernumbers and self.smallernumbers:
+            if (-1 * self.smallernumbers[0]) > self.largernumbers[0]:
+                heapq.heappush(self.largernumbers, -heapq.heappop(self.smallernumbers)) 
         if len(self.smallernumbers) > len(self.largernumbers) + 1:
-            maxofsmallernumbers = -1 * heapq.heappop(self.smallernumbers)
-            heapq.heappush(self.largernumbers, maxofsmallernumbers)
+            val = -heapq.heappop(self.smallernumbers)
+            heapq.heappush(self.largernumbers, val)
         if len(self.largernumbers) > len(self.smallernumbers) + 1:
-            minoflargernumbers = heapq.heappop(self.largernumbers)
-            heapq.heappush(self.smallernumbers, -minoflargernumbers)
-    
-
-
-
-
-    def findMedian(self) -> float:
+            val = heapq.heappop(self.largernumbers)
+            heapq.heappush(self.smallernumbers, -val)
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        #print(self.smallernumbers)
+       # print(self.largernumbers)
         if len(self.smallernumbers) > len(self.largernumbers):
-            return -self.smallernumbers[0]
-        if len(self.largernumbers) > len(self.smallernumbers):
+            val = -1 * self.smallernumbers[0]
+            return val
+            
+        elif len(self.largernumbers) > len(self.smallernumbers):
             return self.largernumbers[0]
         else:
-            return (-self.smallernumbers[0] + self.largernumbers[0])/2
+            maxofsmall = -self.smallernumbers[0]
+            minoflarge = self.largernumbers[0]
+            return (float(maxofsmall) + float(minoflarge))/2
+
+
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)

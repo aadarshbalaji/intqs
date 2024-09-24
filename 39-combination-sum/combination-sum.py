dc52e75d40117dfced5143158cd608e63ff3b562
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-23 19:03:16 -0700

    Time: 49 ms (73.86%) | Memory: 16.6 MB (78.35%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         globalarr = []
-        def canmake(listnum):
-            if sum(listnum) > target:
+        def canmake(path, index):
+            if sum(path) > target:
                 return False
-            elif sum(listnum) == target:
-                currlistnum = sorted(listnum)
-                if currlistnum in globalarr:
-                    return False
-                globalarr.append(sorted(listnum))
-                return True
-            for num in candidates:
-                canmake(listnum + [num])
-        for num in candidates:
-            canmake([num])
+            elif sum(path) == target:
+                globalarr.append(sorted(path))
+            else:
+                for i in range(index, len(candidates)):
+                    canmake(path+[candidates[i]], i)
+        canmake([],0)
         return globalarr
             

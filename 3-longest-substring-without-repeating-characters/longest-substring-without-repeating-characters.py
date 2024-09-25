bdde8607c24bfb962130a12008c6d3befe0651cc
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-25 12:57:10 -0700

    Time: 33 ms (99.91%) | Memory: 16.6 MB (42.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,9 +3,11 @@ class Solution:
         l = 0
         seen = {}
         currmax = 0
+        if len(s) == len(set(s)):
+            return len(s)
         for r in range(len(s)):
             if s[r] in seen and seen[s[r]] >= l:
                 l = seen[s[r]] + 1
-            currmax = max(currmax, r-l +1)
+            currmax = max(currmax, r-l+1)
             seen[s[r]] = r
         return currmax

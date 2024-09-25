28cbe77f2facdbce60e529e0c987b085533abf9d
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-25 11:47:31 -0700

    Time: 36 ms (92.64%) | Memory: 16.6 MB (64.83%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoCitySchedCost(self, costs):
-        """
-        :type costs: List[List[int]]
-        :rtype: int
-        """
-
-        costs.sort(key = lambda x: x[0]-x[1])
-        total = 0
-        n = len(costs)//2
-        for i in range(n):
-            total += costs[i][0]
-            total += costs[n+i][1]
-        return total
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0]-x[1])
+        totalcosts = 0
+        for i in range(len(costs)//2):
+            totalcosts += costs[i][0]
+            totalcosts += costs[len(costs)-1-i][1]
+        return totalcosts

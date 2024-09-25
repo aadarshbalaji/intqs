d4913c0588312914dd46115cc66a9515c2d8d223
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-25 11:52:20 -0700

    Time: 68 ms (95.60%) | Memory: 21.2 MB (19.40%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -3,13 +3,14 @@ class Solution:
         stack = [['', 0]]
         for c in s:
             if stack[-1][0] == c:
-                stack[-1][1] += 1
-                if stack[-1][1] == k:
-                    stack.pop(-1)
+                if stack[-1][1] == k-1:
+                    stack.pop()
+                else:
+                    stack[-1][1] += 1
             else:
                 stack.append([c,1])
-        
-        ans = ''
-        for s,c in stack:
-            ans += s*c
+        ans = ""
+        while stack:
+            s, n = stack.pop()
+            ans = s * n + ans
         return ans

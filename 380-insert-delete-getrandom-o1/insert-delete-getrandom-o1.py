dbd9ebda190858c533fb2bc5c3b7bf2af82f2200
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-25 11:25:32 -0700

    Time: 298 ms (40.46%) | Memory: 63.9 MB (18.15%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,34 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
+        self.s = []
         self.hs = {}
-        self.arr = []
 
     def insert(self, val: int) -> bool:
         if val in self.hs:
             return False
-        self.hs[val] = len(self.arr)
-        self.arr.append(val)
-        return True
-
+        else:
+            self.hs[val] = len(self.s)
+            self.s.append(val)
+            return True
 
     def remove(self, val: int) -> bool:
         if val not in self.hs:
             return False
-        last = self.arr[-1]
-        index = self.hs[val]
-        self.arr[index] = last
-        self.hs[last] = index
-        self.arr[-1] = val
-        self.arr.pop()
-        self.hs.pop(val)
-        return True
-        
+        else:
+            index = self.hs[val]
+            lastval = self.s[-1]
+            self.s[index] = lastval
+            self.hs[lastval] = index
+            self.s.pop(-1)
+            self.hs.pop(val)
+            return True
 
     def getRandom(self) -> int:
-        #print(self.arr)
-        ind = random.randint(0,len(self.arr)-1)
-        return self.arr[ind]
+        index = random.randint(0,len(self.s)-1)
+        return self.s[index]
 
 
 # Your RandomizedSet object will be instantiated and called as such:

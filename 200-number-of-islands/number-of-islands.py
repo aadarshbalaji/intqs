499d5ab77d4345a41f5d92a70c1b53dc7e3543d6
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-26 01:48:33 -0700

    Time: 307 ms (38.09%) | Memory: 35.5 MB (5.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,30 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        lenrows = len(grid)
-        lencols = len(grid[0])
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        numrows = len(grid)
+        numcols = len(grid[0])
         islands = 0
         visited = set()
 
-        def bfs(row, col):
+        def bfs(r,c):
             q = deque()
-            visited.add((row,col))
-            q.append((row,col))
+            visited.add((r,c))
+            q.append((r,c))
             while q:
-                directions = [[0,1], [0,-1],[1,0],[-1,0]]
+                directions = [[0,1], [0,-1], [1,0], [-1,0]]
                 for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
-                    if 0 <= nr < lenrows and 0 <= nc < lencols and (nr,nc) not in visited and grid[nr][nc] == '1':
-                        bfs(nr,nc)
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < numrows) and (0 <= nc < numcols) and (nr,nc) not in visited and grid[nr][nc] == '1':
+                        bfs(nr, nc)
                     visited.add((nr,nc))
                 q.popleft()
 
-        for r in range(lenrows):
-            for c in range(lencols):
+
+        for r in range(numrows):
+            for c in range(numcols):
                 if grid[r][c] == '1' and (r,c) not in visited:
                     islands += 1
                     bfs(r,c)

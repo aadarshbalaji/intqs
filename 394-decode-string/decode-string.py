ca39740d0e2e59256c3834b75e4fe1d8f6453b82
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-27 20:58:32 -0700

    Time: 19 ms (14.70%) | Memory: 11.5 MB (95.68%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,8 +1,12 @@
-class Solution:
-    def decodeString(self, s: str) -> str:
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         stack = []
-        currstring = ''
         currnum = 0
+        currstring = ''
         for c in s:
             if c.isdigit():
                 currnum = currnum * 10 + int(c)
@@ -14,11 +18,10 @@ class Solution:
             elif c == ']':
                 string = stack.pop()
                 num = stack.pop()
-                currstring = string + currstring * num
+                currstring = string + currstring*num
             else:
                 currstring += c
         
-        while stack:
-            currstring = stack.pop() + currstring
-        
         return currstring
+
+        

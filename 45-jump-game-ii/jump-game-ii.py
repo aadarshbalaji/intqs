7d08db09c6a5153417b0eb839310d050f400c5ce
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-30 12:10:51 -0700

    Time: 88 ms (62.74%) | Memory: 12.5 MB (72.13%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,14 +1,17 @@
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        near = far = jumps = 0
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        count = minreach = maxreach = 0
+        while maxreach < n-1:
+            currfar = 0
+            for i in range(minreach, maxreach+1):
+                currfar = max(currfar, i + nums[i])
+            count += 1
+            minreach = maxreach + 1
+            maxreach = currfar
+        return count
 
-        while far < len(nums) - 1:
-            farthest = 0
-            for i in range(near, far + 1):
-                farthest = max(farthest, i + nums[i])
-            
-            near = far + 1
-            far = farthest
-            jumps += 1
-        
-        return jumps

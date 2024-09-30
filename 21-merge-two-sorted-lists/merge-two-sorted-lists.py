42c573c8dac7350a839646cae0cb9a08dc04fcaa
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-29 21:19:34 -0700

    Time: 15 ms (83.80%) | Memory: 11.5 MB (89.09%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,28 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        rv = ListNode()
-        start = rv
-        while list1 != None and list2 != None:
-            x = list1.val
-            y = list2.val
-            if x <= y:
-                rv.next = ListNode(x)
-                rv = rv.next
-                list1 = list1.next
-            elif y < x:
-                rv.next = ListNode(y)
-                rv = rv.next
-                list2 = list2.next
-        if list1 != None:
-            rv.next = list1
-        if list2 != None:
-            rv.next = list2
-        return start.next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(-1, None)
+        curr = dummy
+        a = list1
+        b = list2
+        while a or b:
+            if a and not b:
+                while a:
+                    curr.next = ListNode(a.val, None)
+                    curr = curr.next
+                    a = a.next
+            elif b and not a:
+                while b:
+                    curr.next = ListNode(b.val, None)
+                    curr = curr.next
+                    b = b.next
+            elif a.val > b.val:
+                curr.next = ListNode(b.val, None)
+                curr = curr.next
+                b = b.next
+            else:
+                curr.next = ListNode(a.val, None)
+                curr = curr.next
+                a = a.next
+        return dummy.next
+
+                
+
             
-            
-        

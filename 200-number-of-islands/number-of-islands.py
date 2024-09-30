04b6940a2ad8f6ecc534dfc4085c0609b5f140e9
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-09-29 22:58:33 -0700

    Time: 318 ms (9.64%) | Memory: 30.9 MB (5.10%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,26 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         lenrows = len(grid)
         lencols = len(grid[0])
-        visited = set()
         islands = 0
-        directions = [[0,1],[1,0],[-1,0],[0,-1]]
-        def bfs(row,col):
+        visited = set()
+        def search(row, col):
             q = deque()
-            q.append((r,c))
+            q.append((row,col))
             visited.add((row,col))
             while q:
-                for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
+                directions = [[0,1],[0,-1],[1,0],[-1,0]]
+                for dr , dc in directions:
+                    nr, nc = row  + dr, col + dc
                     if 0 <= nr < lenrows and 0 <= nc < lencols and (nr, nc) not in visited and grid[nr][nc] == '1':
-                        bfs(nr,nc)
+                        search(nr, nc)
                     visited.add((nr,nc))
                 q.popleft()
+
 
         for r in range(lenrows):
             for c in range(lencols):
                 if (r,c) not in visited and grid[r][c] == '1':
                     islands += 1
-                    bfs(r,c)
-                
+                    search(r,c)
         return islands

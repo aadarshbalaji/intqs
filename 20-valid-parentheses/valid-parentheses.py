9ba023426c90f1304c458aece6f45718930f0482
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-10-25 00:09:56 -0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.86%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = deque()
-        op = ['(', '[', '{']
-        cl = [')', ']', '}']
-        for character in s:
-            if character in op:
-                stack.append(character)
-            else:
-                if len(stack) == 0:
+        o = ['(', '{' , '[']
+        c = [')', '}', ']']
+        mapping = {y:x for x,y in zip(o,c)}
+    
+        q = deque()
+        for c in s:
+            if c in o:
+                q.append(c)
+            elif c in c:
+                if len(q) == 0:
                     return False
-                check = stack.pop()
-                if check == op[cl.index(character)]:
-                    continue
-                else:
+                if mapping[c] != q.pop():
                     return False
-        if len(stack) > 0:
-            return False
-        else:
+        if len(q) == 0:
             return True
+        else:
+            return False
+            

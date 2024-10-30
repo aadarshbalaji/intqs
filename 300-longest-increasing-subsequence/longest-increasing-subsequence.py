d0644460bae344354b0be2f678997bac6eebbdae
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-10-30 13:34:34 -0700

    Time: 3 ms (99.03%) | Memory: 12 MB (54.43%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,10 +1,14 @@
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         res = []
         for num in nums:
             if not res or num > res[-1]:
                 res.append(num)
-                continue
-            index = bisect_left(res, num)
-            res[index] = num
+            else:
+                index = bisect_left(res, num)
+                res[index] = num
         return len(res)

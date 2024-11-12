af9db21fa46c7217a1eae11550e3865dd68a68ba
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-11 22:09:49 -0800

    Time: 4 ms (75.71%) | Memory: 14 MB (66.47%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key=lambda x:x[0])
         heap = [intervals[0][1]]
         for start, end in intervals[1:]:
             if start >= heap[0]:
                 heappop(heap)
-                heappush(heap, end)
-            else:
-                heappush(heap, end)
+            heappush(heap, end)
         return len(heap)

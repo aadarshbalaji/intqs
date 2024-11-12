6dfdf57092fccb8ed3ba3799da522abd5fa3dc7e
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-11 20:39:10 -0800

    Time: 12 ms (92.27%) | Memory: 12.6 MB (65.31%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,22 +1,12 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        final = len(nums) - 1
-        i = 0
-        if len(nums) == 1:
-            return True
-        while nums[i] != 0:
-            if i >= final:
-                return True
-            new = 0
-            for j in range(0,nums[i]):
-                j += 1
-                if i + j >= final:
-                    return True
-                if nums[i+j] > (nums[i]-j):
-                    new = i + j
-                    break
-            if new != 0:
-                i = new
-            else:
-                i += nums[i]
-        return False
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        lastseen = len(nums) - 1
+        for i in range(len(nums)-2, -1,-1):
+            if i + nums[i] >= lastseen:
+                lastseen = i
+        return lastseen == 0

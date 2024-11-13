7c945d403008cfde50407c19e0d847bf5e733102
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-12 17:03:36 -0800

    Time: 24 ms (81.70%) | Memory: 13.7 MB (95.26%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if root is None:
+            return root
+        q = deque()
+        q.append(root)
+        while q:
+            size = len(q)
+            n = []
+            for i in range(size):
+                n.append(q.popleft())
+            for i in range(0, len(n)-1):
+                n[i].next = n[i+1]
+            for nnn in n:
+                if nnn.left:
+                    q.append(nnn.left)
+                if nnn.right:
+                    q.append(nnn.right)
+        return root
+        
+
+        

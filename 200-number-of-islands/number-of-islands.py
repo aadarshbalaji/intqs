c99f4340e79b4a4bc6fab259130a6095699023d1
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-13 15:53:27 -0800

    Time: 274 ms (23.75%) | Memory: 24.5 MB (19.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,19 +2,28 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         lenrows = len(grid)
         lencols = len(grid[0])
-        islands = 0
+        count = 0
         visited = set()
-        def search(row, col):
-                directions = [[0,1],[0,-1],[1,0],[-1,0]]
-                for dr, dc in directions:
-                    nr, nc = row  + dr, col + dc
-                    if 0 <= nr < lenrows and 0 <= nc < lencols and (nr, nc) not in visited and grid[nr][nc] == '1':
-                        grid[nr][nc] = '0'
-                        search(nr,nc)
+
+        def search(orow, ocol):
+            if (orow, ocol) in visited:
+                return
+            visited.add((orow,ocol))
+            dirs = [[0,1],[1,0],[0,-1],[-1,0]]
+            for dr, dc in dirs:
+                newrow, newcol = orow + dr, ocol + dc
+                if 0 <= newrow < lenrows and 0 <= newcol < lencols and grid[newrow][newcol] == '1':
+                    search(newrow, newcol)
+                    visited.add((newrow, newcol))  
+
+            
+                          
+
+
 
         for r in range(lenrows):
             for c in range(lencols):
-                if (r,c) not in visited and grid[r][c] == '1':
-                    islands += 1
-                    search(r,c)
-        return islands
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    count += 1
+                    search(r, c)
+        return count

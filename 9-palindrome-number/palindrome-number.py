c2a470547d99fca10b9bd400ad521fb74a29bf45
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-14 15:18:30 -0800

    Time: 29 ms (7.92%) | Memory: 11.6 MB (28.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,22 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        string = str(x)
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        arr = []
+        while x:
+            arr.append(x % 10)
+            x = x//10
+        
         l = 0
-        r = len(string) - 1
-        while l < r:
-            if string[l] != string[r]:
+        r = len(arr) - 1
+        while l <= r:
+            if arr[l] != arr[r]:
                 return False
             l += 1
-            r -=1
+            r -= 1
         return True

be7f30815bd74b4afb1ccf7742c200a7cec4a2c9
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-14 22:41:08 -0800

    Time: 3 ms (79.83%) | Memory: 11.6 MB (64.81%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,23 +1,27 @@
-class Solution:
-    def reorganizeString(self, s: str) -> str:
+class Solution(object):
+    def reorganizeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         freq = Counter(s)
+        new = ''
         heap = []
-        for c, times in freq.items():
-            heappush(heap, [-times, c])
+        for letter, count in freq.items():
+            heappush(heap, [-count, letter])
         
-        currstr = ""
-        if not heap:
-            return ""
-        currnotallowed = heappop(heap)
-        currstr += currnotallowed[1]
-        while len(currstr) != len(s):
-            if not heap:
-                return ""
-            timesleft, newletter = heappop(heap)
-            currstr += newletter
-            if -1 * currnotallowed[0] > 1:
-                heappush(heap, [currnotallowed[0]+1, currnotallowed[1]])
-            currnotallowed = [timesleft, newletter]
-        return currstr
-            
-            
+        if heap:
+            first = heappop(heap)
+            new += first[1]
+            temp = [first[0] + 1, first[1]]
+
+        while heap:
+            curr = heappop(heap)
+            new += curr[1]
+            if temp[0] < 0:
+                heappush(heap, temp)
+            temp = [curr[0] + 1, curr[1]]
+        if len(new) == len(s):
+            return new
+        else:
+            return ''

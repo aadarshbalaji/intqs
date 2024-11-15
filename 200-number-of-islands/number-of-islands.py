5af6478c3bedccb3b8082f10303b2fcf020fc3e6
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-14 23:58:48 -0800

    Time: 257 ms (37.90%) | Memory: 24.5 MB (19.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        lenrows = len(grid)
-        lencols = len(grid[0])
-        count = 0
+        numrows = len(grid)
+        numcols = len(grid[0])
+        total = 0
         visited = set()
-
-        def search(orow, ocol):
-            if (orow, ocol) in visited:
-                return
-            visited.add((orow,ocol))
-            dirs = [[0,1],[1,0],[0,-1],[-1,0]]
-            for dr, dc in dirs:
-                newrow, newcol = orow + dr, ocol + dc
-                if 0 <= newrow < lenrows and 0 <= newcol < lencols and grid[newrow][newcol] == '1':
-                    search(newrow, newcol)
-                    visited.add((newrow, newcol))  
-
-            
-                          
+        def search(r,c):
+            q = deque()
+            q.append((r,c))
+            visited.add((r,c))
+            directions = [[0,1], [0,-1], [1,0], [-1,0]]
+            while q:
+                currR, currC = q.popleft()
+                for dr, dc in directions:
+                    newR, newC = currR + dr, currC + dc
+                    if 0 <= newR < numrows and 0 <= newC < numcols and grid[newR][newC] == '1' and (newR,newC) not in visited:
+                        q.append((newR,newC))
+                        visited.add((newR,newC))
 
 
-
-        for r in range(lenrows):
-            for c in range(lencols):
+        for r in range(numrows):
+            for c in range(numcols):
                 if grid[r][c] == '1' and (r,c) not in visited:
-                    count += 1
-                    search(r, c)
-        return count
+                    total += 1
+                    search(r,c)
+        return total

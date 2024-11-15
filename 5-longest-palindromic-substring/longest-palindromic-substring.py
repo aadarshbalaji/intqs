31fea129746eae642434cae2458cf81e239232ba
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-14 22:56:40 -0800

    Time: 331 ms (70.59%) | Memory: 13.4 MB (15.98%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -6,16 +6,13 @@ class Solution(object):
         """
         if not s:
             return ""
-        curr = ''
         def expand(i, j):
             l = i
             r = j
-            curr = ''
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                curr = max(curr, s[l:r+1], key=len)
                 l -= 1
                 r += 1
-            return curr
+            return s[l+1:r]
         
         return max([max(expand(k,k),expand(k, k+1), key=len) for k in range(len(s))], key=len)
                 

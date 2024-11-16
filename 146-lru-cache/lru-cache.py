8bdaf786152adf5b04783bf62b95783d77491e22
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-16 14:26:17 -0800

    Time: 289 ms (53.00%) | Memory: 77.6 MB (67.73%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,22 +1,68 @@
-class LRUCache:
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.val = value
+        self.prev = None
+        self.next = None
 
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.hs = OrderedDict()
+class LRUCache(object):
 
-    def get(self, key: int) -> int:
-        if key in self.hs:
-            self.hs.move_to_end(key)
-            return self.hs[key]
-        else:
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.keytoNode = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.length = 0
+        self.capacity = capacity
+    
+    def addtotail(self, key, val):
+        node = Node(key, val)
+        node.prev = self.tail.prev
+        self.tail.prev.next = node
+        node.next = self.tail
+        self.tail.prev = node
+        self.keytoNode[key] = node
+        self.length += 1
+    
+    def delete(self, k):
+        node = self.keytoNode[k]
+        del self.keytoNode[k]
+        node.prev.next=node.next
+        node.next.prev = node.prev
+        self.length -= 1
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.keytoNode:
             return -1
+        node = self.keytoNode[key]
+        rv = node.val
+        self.delete(key)
+        self.addtotail(key, rv)
+        return rv
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.hs:
-            self.hs.move_to_end(key)
-        self.hs[key] = value
-        if len(self.hs) > self.cap:
-            self.hs.popitem(False)
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.keytoNode:
+            self.delete(key)
+            self.addtotail(key, value)
+            return
+        if self.length == self.capacity:
+            sample = self.head.next.key
+            self.delete(sample)
+        self.addtotail(key,value)
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

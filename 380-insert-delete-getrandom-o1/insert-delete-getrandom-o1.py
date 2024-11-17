5c2b839fc7a038da9ea2128a63bae705f2876f68
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-16 17:55:52 -0800

    Time: 280 ms (36.85%) | Memory: 57.5 MB (26.23%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,31 +1,43 @@
-class RandomizedSet:
-
+class RandomizedSet(object):
+    import random 
     def __init__(self):
-        self.hs = {} #maps value to index
-        self.arr = [] #holds values
+        self.hs = {}
+        self.arr = []
 
-    def insert(self, val: int) -> bool:
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
         if val in self.hs:
             return False
         self.hs[val] = len(self.arr)
         self.arr.append(val)
         return True
+        
 
-    def remove(self, val: int) -> bool:
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
         if val not in self.hs:
             return False
-        valindex = self.hs[val]
-        lastval = self.arr[-1]
-        self.hs[lastval] = valindex
-        self.arr[valindex] = lastval
-        self.arr.pop(-1)
-        self.hs.pop(val)
+        index = self.hs[val]
+        temp = self.arr[-1]
+        self.arr[index] = temp
+        self.hs[temp] = index
+        self.arr.pop()
+        del self.hs[val]
         return True
 
-
-    def getRandom(self) -> int:
-        index = random.randint(0,len(self.arr)-1)
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        index = random.randint( 0, len(self.arr)-1)
         return self.arr[index]
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:

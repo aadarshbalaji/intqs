9b881a89d2fa4e7ec4a5aae7a63787e90db2688f
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-19 20:26:37 -0800

    Time: 7 ms (54.29%) | Memory: 23.2 MB (6.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,17 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        freq = defaultdict(int)
-        for num in nums:
-            freq[num] += 1
-        heap = []
-        for num, freq in freq.items():
-            heappush(heap, [freq, num])
-            if len(heap) > k:
-                heappop(heap)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #array method O(n) time complexity
+        arr = [[] for i in range(len(nums)+ 1)] 
+        freq = Counter(nums)
+        for num, count in freq.items():
+            arr[count].append(num)
         rv = []
-        for i in range(k):
-            rv.append(heappop(heap)[1])
+        #print(arr)
+        for i in range(len(arr) -1, -1,-1):
+            if k == 0:
+                return rv
+            if arr[i]:
+                k -= len(arr[i])
+                rv.extend(arr[i])
         return rv
+                 

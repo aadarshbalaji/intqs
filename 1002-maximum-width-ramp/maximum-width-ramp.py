3f7578b15150bcc709eca6be2c24c4ac8a3f84bc
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-21 00:01:12 -0800

    Time: 80 ms (46.99%) | Memory: 23 MB (96.99%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,15 +1,17 @@
-class Solution(object):
-    def maxWidthRamp(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        call = (list(range(len(nums))))
-        call.sort(key = lambda x: nums[x])
-        #print(call)
-        currmin = float('inf')
-        maxrange = 0
-        for i in range(len(call)):
-            maxrange = max(maxrange, call[i]-currmin)
-            currmin = min(currmin, call[i])
-        return maxrange
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        n = len(nums)
+        right_max = [None] * n
+
+        # Fill right_max array with the maximum values from the right
+        right_max[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], nums[i])
+        l = 0
+        currmax = 0
+        for r in range(len(right_max)):
+
+            while l < r and nums[l] > right_max[r]:
+                l += 1
+            currmax = max(currmax, r-l)
+        return currmax

57a251d86acac17c42e9b2bbbf8e64c0467a769d
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-20 22:57:36 -0800

    Time: 802 ms (8.00%) | Memory: 19 MB (23.83%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxWidthRamp(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        call = (list(range(len(nums))))
+        call.sort(key = lambda x: nums[x])
+        print(call)
+        currmin = float('inf')
+        maxrange = 0
+        for i in range(len(call)):
+            maxrange = max(maxrange, call[i]-currmin)
+            currmin = min(currmin, call[i])
+        return maxrange

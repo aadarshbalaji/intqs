5d16c53a220efd5c999935fec242b2afcb4358f9
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-20 22:05:26 -0800

    Time: 0 ms (100.00%) | Memory: 11.8 MB (10.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,10 +1,19 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        def rob1(smol):
-            r1,r2 = 0,0
-            for num in smol:
-                temp = max(num + r1, r2)
-                r1 = r2
-                r2 = temp
-            return r2
-        return max(nums[0] + rob1(nums[2:len(nums)-1]), rob1(nums[1:]))
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 3:
+            return max(nums)
+        def helper(arr):
+            if not arr:
+                return 0
+            dp = [0] * len(arr)
+            dp[0] = arr[0]
+            for i in range(1, len(arr)):
+                dp[i] = max(dp[i-2] + arr[i], dp[i-1])
+            return dp[-1]
+        return max(helper(nums[1:]), helper(nums[:-1]))
+            
+        

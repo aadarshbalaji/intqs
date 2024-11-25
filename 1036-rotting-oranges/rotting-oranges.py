eb9f133d04afd28fb16709b9aca4f4d77c915154
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-25 03:10:56 -0800

    Time: 3 ms (81.68%) | Memory: 16.5 MB (98.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -4,25 +4,28 @@ class Solution:
         lencols = len(grid[0])
         numfresh = 0
         timer = 0
-        directions = [[0,1],[0,-1], [1,0],[-1,0]]
+        directions = [[0,1], [0,-1],[1,0],[-1,0]]
         q = deque()
         for i in range(lenrows):
             for j in range(lencols):
-                if grid[i][j] == 1:
-                    numfresh += 1
                 if grid[i][j] == 2:
                     q.append((i,j))
-        if not numfresh:
+                if grid[i][j] == 1:
+                    numfresh += 1
+
+        if numfresh == 0:
             return 0
+        
         while q:
-            lq = len(q)
-            for i in range(lq):
-                row, col = q.popleft()
+            if numfresh == 0:
+                return timer
+            for i in range(len(q)):
+                r, c = q.popleft()
                 for dr, dc in directions:
-                    newrow, newcol = row + dr, col + dc
-                    if 0 <= newrow < lenrows and 0 <= newcol < lencols and grid[newrow][newcol] == 1:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < lenrows and 0 <= nc < lencols and grid[nr][nc] == 1:
                         numfresh -= 1
-                        grid[newrow][newcol] = 2
-                        q.append((newrow, newcol))
+                        q.append((nr,nc))
+                        grid[nr][nc] = 2
             timer += 1
-        return -1 if numfresh else timer-1
+        return -1

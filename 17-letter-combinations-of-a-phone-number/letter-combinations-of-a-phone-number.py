8701bdf19f0a6b5b878f66d449b7a0ce67d2bc2c
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-25 09:47:01 -0800

    Time: 0 ms (100.00%) | Memory: 16.8 MB (11.61%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        mapping = {2: 'abc', 3: 'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
-        rv=[]
-        def backtrack(index, path):
-            if index == len(digits):
-                rv.append(str(path))
-                return
-            choices = [s for s in mapping[int(digits[index])]]
-            for c in choices:
-                new = path + c
-                backtrack(index + 1, new)
-        
-        backtrack(0, '')
-        if not rv or (len(rv)==1 and rv[0] == ""):
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
             return []
+        combos = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
+        rv = []
+        def helper(index, curr):
+            if index >= len(digits):
+                rv.append(str(curr))
+                return
+            for c in combos[int(digits[index])]:
+                curr += c
+                helper(index+1, curr)
+                curr = curr[:-1]
+
+        helper(0, '')
         return rv

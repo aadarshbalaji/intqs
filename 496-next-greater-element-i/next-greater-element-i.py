5eda1588ae3ccccc7bfd797eef7995aec1806e39
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-25 18:54:39 -0800

    Time: 2 ms (63.22%) | Memory: 11.8 MB (46.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,10 +1,20 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        rv = {}
-        st = []
-        for num in reversed(nums2):
-            while st and st[-1] <= num:
-                st.pop()
-            rv[num] = st[-1] if st else -1
-            st.append(num)
-        return [rv[num] for num in nums1]
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        rv = [-1] * len(nums1)
+        hs = {}
+        for j, num1 in enumerate(nums1):
+            hs[num1] = j
+        stack = []
+        for i, num in enumerate(nums2):
+            while stack and num > stack[-1]:
+                rv[hs[stack.pop()]] = num
+            if num in hs:
+                stack.append(num)
+        return rv
+            
+

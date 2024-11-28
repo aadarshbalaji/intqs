f04b0a710aa999d992a8384560106bdbb61d48a7
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-11-27 16:49:55 -0800

    Time: 171 ms (71.97%) | Memory: 69.8 MB (93.13%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,28 +1,44 @@
-class TimeMap:
+class TimeMap(object):
 
     def __init__(self):
-        self.hashmap = defaultdict(list)
+        self.word_to_times = defaultdict(list)
+        self.word_to_time_to_val = defaultdict(dict)
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.hashmap[key].append([timestamp, value])
+    def set(self, key, value, timestamp):
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        self.word_to_times[key].append(timestamp)
+        self.word_to_time_to_val[key][timestamp] = value
 
-    def get(self, key: str, timestamp: int) -> str:
-        currlist = self.hashmap[key]
-        left, right = 0, len(currlist) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if currlist[mid][0] == timestamp:
-                return currlist[mid][1]
-            elif currlist[mid][0] < timestamp:
-                left = mid + 1  # Move right
+        
+
+    def get(self, key, timestamp):
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        if key not in self.word_to_times or self.word_to_times[key][0] > timestamp:
+            return ''
+        arr = self.word_to_times[key]
+        l = 0 
+        r = len(arr)-1
+        fin = l
+        while l <= r:
+            mid = (l + r) //2
+            if arr[mid] == timestamp:
+                fin = mid
+                break
+            if arr[mid] > timestamp:
+                r = mid - 1
             else:
-                right = mid - 1  # Move left
-        if right >= 0:
-            return currlist[right][1]
-        
-        return ""
-    
-        
+                fin = mid
+                l = mid + 1
+        return self.word_to_time_to_val[key][arr[fin]]
 
 
 # Your TimeMap object will be instantiated and called as such:

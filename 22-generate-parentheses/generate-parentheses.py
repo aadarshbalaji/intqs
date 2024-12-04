c632f189b98cd9e60dc853faf4829985c30f9b06
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-04 03:02:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (6.09%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
         rv = []
         def helper(o, c, curr):
             if o == c == n:
                 rv.append(str(curr))
-            if o < n:
-                helper(o+1, c, curr + '(')
+                return
             if c < o <= n:
-                helper(o, c + 1, curr + ')')
+                helper(o, c+1, curr+')')
+            if o < n:
+                helper(o+1, c, curr+'(')
         helper(0,0,'')
         return rv

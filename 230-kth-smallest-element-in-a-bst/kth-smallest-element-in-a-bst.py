388779020c247f26c3bb23c5d76fd8b923153eae
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-03 21:52:29 -0500

    Time: 3 ms (35.51%) | Memory: 20.6 MB (6.96%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,27 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        arr = []
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        curr = []
         def search(node):
-            #left middle right
-            if node.left:
-                search(node.left)
-            if node:
-                arr.append(node.val)
-                if len(arr) >= k:
-                    return arr[-1]
-            if node.right:
-                search(node.right)
+            if not node:
+                return
+            search(node.left)
+            curr.append(node.val)
+            if len(curr) == k:
+                return curr[-1]
+            search(node.right)
         search(root)
-        return arr[k-1]
-        
+        return curr[k-1]

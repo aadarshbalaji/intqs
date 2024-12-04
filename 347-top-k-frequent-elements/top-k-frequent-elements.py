50b641cf24c261e7a94c0bdf67cbf18d55720e71
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-03 19:33:07 -0500

    Time: 11 ms (57.26%) | Memory: 19.9 MB (7.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,25 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #array method O(n) time complexity
-        arr = [[] for i in range(len(nums)+ 1)] 
-        freq = Counter(nums)
-        for num, count in freq.items():
-            arr[count].append(num)
-        rv = []
-        #print(arr)
-        for i in range(len(arr) -1, -1,-1):
-            if k == 0:
-                return rv
-            if arr[i]:
-                k -= len(arr[i])
-                rv.extend(arr[i])
-        return rv
-                 
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = defaultdict(int)
+        for i in nums:
+            count[i] += 1
+       
+        buckets = [[] for i in range(len(nums) + 1)]
+        for num, count in count.items():
+            buckets[count].append(num)
+        
+        arr = []
+        for bucket in buckets[::-1]:
+            if len(arr) < k:
+                arr.extend(bucket)
+        return arr
+
+            
+
+        
+        

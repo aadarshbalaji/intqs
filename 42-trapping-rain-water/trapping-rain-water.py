296433e6d7dc0a1d59bb5e13a3657e9c41106fe0
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-03 19:38:31 -0500

    Time: 39 ms (10.31%) | Memory: 19.2 MB (6.03%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        l = 0
-        r = len(height) - 1
-        maxL = height[0]
-        maxR = height[-1]
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+        prefix = [0] * (len(height) + 1)
+        suff = [0] * (len(height) + 1)
+        for i, pre in enumerate(height[1:]):
+            prefix[i+1] = max(prefix[i], height[i])
+        for i in range(len(height)-2,-1,-1):
+            suff[i] = max(suff[i+1], height[i+1])
         total = 0
-        while l < r:
-            if maxL <= maxR:
-                l += 1
-                maxL = max(maxL, height[l])
-                total += maxL - height[l]
-            else:
-                r -= 1
-                maxR = max(maxR, height[r])
-                total += maxR - height[r]
+        for i in range(len(height)):
+            total += max(0, min(suff[i], prefix[i]) - height[i])
         return total
+            

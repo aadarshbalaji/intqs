9e18ebe3288f24547fb18918fe02ffc636ead16b
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-05 13:59:29 -0800

    Time: 0 ms (100.00%) | Memory: 12.1 MB (7.41%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,16 +1,55 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        x,y,dx,dy = 0,0,1,0
-        res = []
-        for i in range(rows * cols):
-            res.append(matrix[y][x])
-            matrix[y][x] = '.'
-
-            if not 0 <= x + dx < cols or not 0 <= y + dy < rows or matrix[y+dy][x+dx] == '.':
-                dx, dy = -dy, dx
-            
-            x += dx
-            y += dy
-        return res
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """        
+        r = True
+        d = False
+        l = False
+        u = False
+        rv = []
+        n = len(matrix)
+        m = len(matrix[0])
+        x,y = 0,0
+        visited = set()
+        visited.add((0,0))
+        while len(rv) != n * m:
+            rv.append(matrix[x][y])
+            visited.add((x,y))
+            if r:
+                if (x, y+1) not in visited and y + 1 < m:
+                    y += 1
+                    continue
+                else:
+                    r = False
+                    d = True
+            if d:
+                if (x+1, y) not in visited and x + 1 < n:
+                    x += 1
+                    continue
+                else:
+                    d = False
+                    l = True
+            if l:
+                if (x, y-1) not in visited and y-1 >= 0:
+                    y -= 1
+                    continue
+                else:
+                    l = False
+                    u = True
+            if u:
+                if (x-1, y) not in visited and x-1 >= 0:
+                    x -= 1
+                    continue
+                else:
+                    u = False
+                    r = True
+            if r:
+                if (x, y+1) not in visited and y + 1 < m:
+                    y += 1
+                    continue
+                else:
+                    r = False
+                    d = True
+        return rv

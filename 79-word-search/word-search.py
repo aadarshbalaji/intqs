d2e1d59bcfeaa84dc93a6f0deb081f3e9b264da8
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-05 11:34:30 -0800

    Time: 4767 ms (32.69%) | Memory: 17.6 MB (5.54%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,21 +3,22 @@ class Solution:
         lenrows = len(board)
         lencols = len(board[0])
         visited = set()
-        def search(r, c, index):
+
+        def search(index, i, j):
             if index == len(word):
                 return True
-            if (r,c) in visited or not 0 <= r < lenrows or not 0 <= c <lencols or board[r][c] != word[index]:
+            if not 0 <= i < lenrows or not 0 <= j < lencols or (i, j) in visited or board[i][j] != word[index]:
                 return False
-            visited.add((r,c))
-            rv = search(r+1, c, index+1) or search(r-1, c, index+1) or search(r, c-1, index+1) or  search(r, c+1, index+1)
-            visited.remove((r,c))
-            return rv
+            visited.add((i,j))
+            ans = search(index+1, i+1, j) or search(index+1, i-1, j) or search(index+1, i, j-1) or search(index+1, i, j+1)
+            visited.remove((i,j))
+            return ans
 
 
-
-        for i in range(lenrows):
-            for j in range(lencols):
-                if board[i][j] == word[0]:
-                    if search(i, j, 0):
+        for x in range(lenrows):
+            for y in range(lencols):
+                if board[x][y] == word[0]:
+                    if search(0, x,y):
                         return True
         return False
+

7e6abfc2e3108b3f5b0a2d2e9afa25a565d011c3
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-05 22:28:55 -0800

    Time: 40 ms (7.78%) | Memory: 18.9 MB (10.03%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,15 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height) < 3:
-            return 0
-        prefix = [0] * (len(height) + 1)
-        suff = [0] * (len(height) + 1)
-        for i, pre in enumerate(height[1:]):
-            prefix[i+1] = max(prefix[i], height[i])
-        for i in range(len(height)-2,-1,-1):
-            suff[i] = max(suff[i+1], height[i+1])
         total = 0
-        for i in range(len(height)):
-            total += max(0, min(suff[i], prefix[i]) - height[i])
+        pre = [0] * (len(height) + 1)
+        suff = [0] * (len(height) + 1)
+        for i in range(1, len(height)):
+            pre[i] = max(pre[i -1], height[i-1])
+        for i in range(len(height)-2,-1,-1):
+            suff[i] = max(suff[i+1],height[i+1])
+        
+        for j, wall in enumerate(height):
+            curr = min(suff[j], pre[j]) - wall
+            total += max(0, curr)
+        
         return total
-            

d2de04b6579ec9ce48595ca7d277fd6dd8554914
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-05 21:36:43 -0800

    Time: 56 ms (43.36%) | Memory: 31 MB (16.41%) - LeetSync

---
+++ 3694-minimum-total-operations/minimum-total-operations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        countdiffs = 0
+        prev = nums[-1]
+        for num in nums[-2::-1]:
+            if prev != num:
+                countdiffs += 1
+            prev = num
+        wow = 0
+        prev = nums[0]
+        for x in nums[1:]:
+            if prev != x:
+                wow += 1
+            prev = x
+        return min(countdiffs, wow)

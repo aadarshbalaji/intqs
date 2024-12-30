fa39179d7d26534fc2c8268817dc10f81de41f86
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2024-12-29 22:54:50 -0500

    Time: 4 ms (20.59%) | Memory: 19.6 MB (26.46%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -5,24 +5,22 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        ans = []
-        i = 0
         start, end = newInterval
-        while i < len(intervals) and intervals[i][1] < start:
-            ans.append(intervals[i])
+        merged = []
+        i=0
+        while i < len(intervals) and start > intervals[i][1]:
+            merged.append(intervals[i])
             i += 1
-        if i < len(intervals):
-            print(intervals[i])
-        curr = newInterval
+
         while i < len(intervals) and end >= intervals[i][0]:
-            curr[0] = min(intervals[i][0], curr[0])
-            curr[1] = max(intervals[i][1], end)
-            i += 1
-
-        ans.append(curr)
-
-        while i < len(intervals):
-            ans.append(intervals[i])
+            start = min(start, intervals[i][0])
+            end = max(end, intervals[i][1])
             i += 1
         
-        return ans
+        merged.append([start,end])
+
+        while i < len(intervals):
+            merged.append(intervals[i])
+            i += 1
+        return merged
+        

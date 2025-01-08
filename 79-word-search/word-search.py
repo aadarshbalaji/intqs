260b1cf76b91cd6c8c13003f1d397002f107b3fa
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-01-07 22:36:08 -0500

    Time: 4669 ms (37.99%) | Memory: 18.2 MB (6.96%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,24 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
         lenrows = len(board)
         lencols = len(board[0])
-        visited = set()
 
-        def search(index, i, j):
-            if index == len(word):
-                return True
-            if not 0 <= i < lenrows or not 0 <= j < lencols or (i, j) in visited or board[i][j] != word[index]:
+        def search(index, r, c, visited):
+            #print(index, r, c, visited)
+            if word[index] != board[r][c]:
                 return False
-            visited.add((i,j))
-            ans = search(index+1, i+1, j) or search(index+1, i-1, j) or search(index+1, i, j-1) or search(index+1, i, j+1)
-            visited.remove((i,j))
-            return ans
+            if index == len(word) -1:
+                return True
+            visited.add((r,c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < lenrows and 0 <= nc < lencols and (nr, nc) not in visited and word[index+1] == board[nr][nc]:
+                    if search(index + 1, nr, nc, visited):
+                        return True
+                    visited.remove((nr,nc))
+            return False
 
-
-        for x in range(lenrows):
-            for y in range(lencols):
-                if board[x][y] == word[0]:
-                    if search(0, x,y):
+        for row in range(lenrows):
+            for col in range(lencols):
+                if board[row][col] == word[0]:
+                    if search(0, row, col, set()):
                         return True
         return False
-

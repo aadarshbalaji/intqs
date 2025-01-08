9ca9ea3cc04103b5e355fdb85527b3446de08674
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-01-07 19:52:08 -0500

    Time: 30 ms (98.42%) | Memory: 18.4 MB (5.26%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,38 +1,40 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+
         if not node:
-            return None
-        hs = {}
-        q = deque()
-        visited = set()
-        q.append(node)
-        while q:
-            n = q.popleft()
-            if n in visited:
-                continue
-            if n not in hs:
-                hs[n] = Node(n.val)
-            visited.add(n)
-            for neigh in n.neighbors:
-                if neigh not in hs:
-                    hs[neigh] = Node(neigh.val)
-                hs[n].neighbors.append(hs[neigh])
-                q.append(neigh)
-                
-        return hs[node]
+            return node
 
+        # Dictionary to save the visited node and it's respective clone
+        # as key and value respectively. This helps to avoid cycles.
+        visited = {}
 
-            
-        
+        # Put the first node in the queue
+        queue = deque([node])
+        # Clone the node and put it in the visited dictionary.
+        visited[node] = Node(node.val, [])
+
+        # Start BFS traversal
+        while queue:
+            # Pop a node say "n" from the from the front of the queue.
+            n = queue.popleft()
+            # Iterate through all the neighbors of the node
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    # Clone the neighbor and put in the visited, if not present already
+                    visited[neighbor] = Node(neighbor.val, [])
+                    # Add the newly encountered node to the queue.
+                    queue.append(neighbor)
+                # Add the clone of the neighbor to the neighbors of the clone node "n".
+                visited[n].neighbors.append(visited[neighbor])
+
+        # Return the clone of the node from visited.
+        return visited[node]

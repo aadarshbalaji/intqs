5da645fbb162c51b1d70dc0e163142ecbfdcc2b9
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-01-08 17:24:22 -0500

    Time: 4 ms (5.11%) | Memory: 17.6 MB (31.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums) < 3:
-            return max(nums)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
         def robsimple(arr):
+            if len(arr) < 3:
+                return max(arr)
             dp = [0] * len(arr)
             dp[0] = arr[0]
             dp[1] = max(arr[0], arr[1])
-            for i in range(2,len(arr)):
-                dp[i] = max(dp[i-2]+arr[i], dp[i-1])
+            for i in range(2, len(arr)):
+                dp[i] = max(dp[i-2] + arr[i], dp[i-1])
             return dp[-1]
+
+
+
         return max(robsimple(nums[1:]), robsimple(nums[:-1]))

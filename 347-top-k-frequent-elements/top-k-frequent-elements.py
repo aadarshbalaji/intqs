66152dec99f330aa9480d75329e628bc52acc5a2
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-01-16 07:46:36 -0500

    Time: 10 ms (64.06%) | Memory: 14.5 MB (37.32%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,21 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        count = defaultdict(int)
-        for i in nums:
-            count[i] += 1
-       
-        buckets = [[] for i in range(len(nums) + 1)]
-        for num, count in count.items():
-            buckets[count].append(num)
-        
-        arr = []
-        for bucket in buckets[::-1]:
-            if len(arr) < k:
-                arr.extend(bucket)
-        return arr
-
-            
-
-        
-        
+        rv = []
+        s = set()
+        h = []
+        c = Counter(nums)
+        for num, occurences in c.items():
+            heappush(h, [-occurences, num])
+        for i in range(k):
+            if h:
+                rv.append(heappop(h)[1])
+        return rv

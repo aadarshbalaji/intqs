fbba8f836ea6fd881aad55eb1d0d410a2a9875f3
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-02-01 00:57:19 -0500

    Time: 121 ms (34.73%) | Memory: 27 MB (26.90%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        currmax = 0
-        currmin = prices[0]
-        for p in prices[1:]:
-            if p > currmin:
-                currmax = max(currmax, p-currmin)
-            else:
-                currmin = p
-        return currmax
+        maxprof = 0
+        lowest = prices[0]
+        for price in prices:
+            maxprof = max(maxprof, price-lowest)
+            lowest = min(lowest, price)
+        return maxprof

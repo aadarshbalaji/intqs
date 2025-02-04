c0d1e6e797d27b93c8983d7acfec53b8edb887c2
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-02-04 15:43:35 -0800

    Time: 565 ms (40.79%) | Memory: 13.5 MB (15.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if len(s) == 1:
+        if len(s) < 2:
             return s
+        
         def explore(i, j):
-            currmax = ''
             l = i
             r = j
-            while l >= 0 and r < len(s) and s[l] == s[r]:
+            curr = ''
+            while l >= 0 and r < len(s) and s[r] == s[l]:
                 l -= 1
                 r += 1
-                currmax = max(currmax, s[l+1: r], key=len)
-            return currmax
-        
-        allexplorations = [max(explore(k,k+1), explore(k,k), key=len) for k in range(len(s)-1)]
-        return max(allexplorations, key=len)
+                curr = s[l+1:r]
+            return curr
+        allperms = [max(explore(k, k), explore(k, k+1), key=len) for k in range(len(s) -1)]
+        return max(allperms, key = len)

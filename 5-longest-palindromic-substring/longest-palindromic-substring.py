abbe371f76b4cc65d395325ab394808cba2b1a11
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-06-15 17:09:46 -0700

    Time: 363 ms (58.83%) | Memory: 13.6 MB (15.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        """
-        find middle index, go outwards
-        """
-        def expand(i, j):
-            state = s[i:j]
+        def explore(i, j):
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                state = s[i:j+1]
                 i -= 1
                 j += 1
-            return state
-        currmax = ""
-        for i in range(len(s)):
-            currmax = max(currmax, expand(i,i), expand(i,i+1), key=len)
-        return currmax
-
+            return s[i+1: j]
+        
+        return max([max(explore(i,i), explore(i,i+1), key=len) for i in range(len(s))], key=len)

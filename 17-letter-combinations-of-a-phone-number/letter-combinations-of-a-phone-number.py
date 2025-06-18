55fa79160805f09417bbc3cc10316a3fd3fd311f
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-06-17 23:04:03 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.63%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        combos = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
-        rv = []
-        def helper(index, curr):
-            if index >= len(digits):
-                rv.append(str(curr))
-                return
-            for c in combos[int(digits[index])]:
-                curr += c
-                helper(index+1, curr)
-                curr = curr[:-1]
+        mapping = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
 
-        helper(0, '')
+        rv = []
+        def explore(currstring, index):
+            if index >= len(digits):
+                if currstring:
+                    rv.append(currstring)
+                return
+            for letter in mapping[digits[index]]:
+                explore(currstring + letter, index+1)
+        
+        explore('', 0)
         return rv
+
+    

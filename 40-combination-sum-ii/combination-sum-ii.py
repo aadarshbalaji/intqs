8baedafc7ada67c79100076772351c89e8d508be
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-07-21 23:33:06 -0700

    Time: 3 ms (97.32%) | Memory: 12.5 MB (67.75%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,19 +1,28 @@
-class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        rv = []
         candidates.sort()
-        ans = []
 
-        def dfs(target, start, comb):
-            if target < 0:
-                return 
-            elif target == 0:
-                ans.append(comb)
+        def backtrack(start, path, remaining):
+            if remaining == 0:
+                rv.append(list(path))
                 return
+            
             for i in range(start, len(candidates)):
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
-                if candidates[i] > target:
-                    break
-                dfs(target-candidates[i], i+1, comb+[candidates[i]])
-        dfs(target, 0, [])
-        return ans
+                
+                if candidates[i] > remaining:
+                    return
+                
+                path.append(candidates[i])
+                backtrack(i + 1, path, remaining - candidates[i])
+                path.remove(candidates[i])
+        
+        backtrack(0,[],target)
+        return rv

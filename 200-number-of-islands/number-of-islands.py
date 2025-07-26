1af9c9583047e89d5e9e25894560275f8a7b347b
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-07-26 11:41:11 -0700

    Time: 248 ms (36.86%) | Memory: 27.7 MB (89.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,26 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         numrows = len(grid)
         numcols = len(grid[0])
-        total = 0
-        visited = set()
-        def search(r,c):
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
-            directions = [[0,1], [0,-1], [1,0], [-1,0]]
-            while q:
-                currR, currC = q.popleft()
-                for dr, dc in directions:
-                    newR, newC = currR + dr, currC + dc
-                    if 0 <= newR < numrows and 0 <= newC < numcols and grid[newR][newC] == '1' and (newR,newC) not in visited:
-                        q.append((newR,newC))
-                        visited.add((newR,newC))
+
+        def explore(row, col):
+            directions = [[0,1],[1,0],[-1,0],[0,-1]]
+            for dr, dc in directions:
+                newrow, newcol = row + dr, col + dc
+                if (0 <= newrow < numrows) and (0 <= newcol < numcols) and grid[newrow][newcol] == '1':
+                    grid[newrow][newcol] = 'V'
+                    explore(newrow, newcol)
 
 
-        for r in range(numrows):
-            for c in range(numcols):
-                if grid[r][c] == '1' and (r,c) not in visited:
-                    total += 1
-                    search(r,c)
-        return total
+
+        count = 0
+        for i in range(numrows):
+            for j in range(numcols):
+                if grid[i][j] == '1':
+                    count += 1
+                    explore(i,j)
+        return count

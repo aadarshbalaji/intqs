9f00919f0b42718ad57fee3817c9856cf7d2839b
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-08-20 18:32:03 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (24.06%) - LeetSync

---
+++ 362-design-hit-counter/design-hit-counter.py
@@ -0,0 +1,27 @@
+class HitCounter(object):
+
+    def __init__(self):
+        self.deq = deque()
+
+    def hit(self, timestamp):
+        """
+        :type timestamp: int
+        :rtype: None
+        """
+        self.deq.append(timestamp)
+        
+
+    def getHits(self, timestamp):
+        """
+        :type timestamp: int
+        :rtype: int
+        """
+        while self.deq and self.deq[0] <= max(0, timestamp - 300):
+            self.deq.popleft()
+        return len(self.deq)
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)

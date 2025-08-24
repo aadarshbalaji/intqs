3ccfb8563935883fe79f9102fa9378af02ea6b7c
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-08-24 16:26:44 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.70%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,20 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        dp = [0] * (len(nums) )
-        if len(nums) < 3:
-            return max(nums)
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        '''
+        dp[i] = max amount u can steal only using houses 0 -> i
+        dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        '''
+        dp = [0 for i in range(len(nums))]
+        if len(nums) < 2:
+            return nums[0] if nums else 0
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        return dp[-1]
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        return dp[len(nums)-1]
+
+        

5ca520c47db47e0f3eb4ad5421cfdbba37c11c68
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-08-24 16:54:39 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) < 3:
-            return max(nums)
-        def helper(arr):
+        def rob_helper(arr):
             dp = [0 for i in range(len(arr))]
             if len(arr) < 3:
                 return max(arr)
             dp[0] = arr[0]
-            dp[1] = max(arr[0], arr[1])
-            for j in range(2, len(arr)):
-                dp[j] = max(dp[j-1], dp[j-2] + arr[j])
+            dp[1] = max(arr[0],arr[1])
+            for i in range(2, len(arr)):
+                dp[i] = max(dp[i-1], dp[i-2] + arr[i])
             return dp[-1]
-        return max(helper(nums[1:]), helper(nums[:-1]))
-
-        
+        if len(nums) < 3:
+            return max(nums)
+        return max(rob_helper(nums[1:]), rob_helper(nums[:-1])) 
+            

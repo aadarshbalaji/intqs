75a5b0fcd79069d62d6075ee90808b266f29c8a0
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-08-24 18:18:01 -0700

    Time: 8 ms (52.00%) | Memory: 13.9 MB (24.00%) - LeetSync

--- 743-closest-leaf-in-a-binary-tree/closest-leaf-in-a-binary-tree.py
+++ 743-closest-leaf-in-a-binary-tree/closest-leaf-in-a-binary-tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import defaultdict
 class Solution(object):
     def findClosestLeaf(self, root, k):
         """
@@ -12,28 +13,37 @@ class Solution(object):
         :rtype: int
         """
         graph = defaultdict(list)
+        q = deque()
+        k_node = [None]
         leaves = set()
         def create_graph(node, parent):
             if not node:
-                return
-            if parent:
-                graph[parent.val].append(node.val)
-                graph[node.val].append(parent.val)
+                return 
+            if node.val == k:
+                k_node[0] = node
             if not node.left and not node.right:
-                leaves.add(node.val)
+                leaves.add(node)
+            if parent:
+                graph[parent].append(node)
+                graph[node].append(parent)
             create_graph(node.left, node)
             create_graph(node.right, node)
+            
         create_graph(root, None)
-        q = deque()
-        q.append(k)
-        seen = set([k])
+        #print(graph)
+        if k_node[0] in leaves:
+            return k_node[0].val
+        q.append(k_node[0])
+        count = 1
+        seen = set()
         while q:
             node = q.popleft()
-            if node in leaves:
-                return node
-            
             seen.add(node)
+            if node in leaves:
+                return node.val
             for neighbor in graph[node]:
                 if neighbor not in seen:
                     q.append(neighbor)
-        return k
+            count += 1
+
+            

03ca2876206a1af77617d2983bb6c88dd8b8acae
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-08-26 17:10:01 -0700

    Time: 1730 ms (7.45%) | Memory: 161.5 MB (70.21%) - LeetSync

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def getDirections(self, root, startValue, destValue):
+        """
+        :type root: Optional[TreeNode]
+        :type startValue: int
+        :type destValue: int
+        :rtype: str
+        """
+
+        def explore(node, path, target):
+            if not node:
+                return ''
+            if node.val == target:
+                return path
+            path.append('L')
+            currleft = explore(node.left, path, target)
+            if currleft:
+                return currleft
+            path.pop()
+            path.append('R')
+            currright = explore(node.right, path, target)
+            if currright:
+                return currright
+            path.pop()
+                    
+        tostart = explore(root, [], startValue)
+        toend = explore(root, [], destValue)
+        print(tostart, toend)
+        
+        i = 0
+        while i < len(tostart) and i < len(toend) and tostart[i] == toend[i]:
+            i += 1
+
+        rv = ['U' for k in range(len(tostart)-i)]
+        for k in range(i, len(toend)):
+            rv.append(toend[k])
+        return ''.join(rv)
+

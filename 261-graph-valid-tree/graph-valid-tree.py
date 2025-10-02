707fce819b1ecd82d4972a1f1e925837ec330ae0
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-10-02 15:37:22 -0700

    Time: 4 ms (27.30%) | Memory: 19.1 MB (52.58%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,21 +1,23 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False
+        
         graph = defaultdict(list)
         for start, end in edges:
             graph[start].append(end)
             graph[end].append(start)
 
         visited = set()
-        visited.add(0)
-        q = deque()
-        q.append(0)
-        while q:
-            curr = q.popleft()
-            for neighbor in graph[curr]:
-                if neighbor in visited:
-                    continue
-                visited.add(neighbor)
-                q.append(neighbor)
-                # Remove the back edge to avoid revisiting the parent node
-                graph[neighbor].remove(curr)
-        return len(visited) == n and len(edges) == n - 1
+        def dfs(node):
+            if node in visited:
+                return 
+            visited.add(node)
+            for neighbor in graph[node]:
+                dfs(neighbor)
+
+        dfs(0)
+        return len(visited) == n
+                
+
+        

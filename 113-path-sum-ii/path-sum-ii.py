8d37e03e55fb0c6e84e36bd9c4cdced4d93b9fdc
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-10-04 21:16:45 -0700

    Time: 3 ms (45.22%) | Memory: 18.6 MB (99.83%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -1,29 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def pathSum(self, root, targetSum):
-        """
-        :type root: Optional[TreeNode]
-        :type targetSum: int
-        :rtype: List[List[int]]
-        """
-        rv = []
-        def traversal(node, path, currsum):
-            if not node:
-                return
-            newsum = currsum + node.val
-            if not node.left and not node.right and newsum == targetSum:
-                rv.append(list(path))
-            if node.left:
-                traversal(node.left, list(path) + [node.left.val], newsum)
-            if node.right:
-                traversal(node.right, list(path) + [node.right.val], newsum)
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return []
-        traversal(root, [root.val], 0)
-        return rv
+        arr = []
+        def helper(node, path):
+            if sum(path) == targetSum and not node.right and not node.left:
+                arr.append(list(path))
+            if not node:
+                return
+            if node.left:
+                path.append(node.left.val)
+                helper(node.left, path)
+                path.pop()
+            if node.right:
+                path.append(node.right.val)
+                helper(node.right, path)
+                path.pop()
+        
+        helper(root, [root.val])
+        return arr
             
+
+            
+                

eb90b18128266ed2302c7daa86cef1f98ec75721
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-10-05 12:33:16 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.08%) - LeetSync

--- 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
@@ -1,52 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def findLeaves(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
 
-        def isleaf(node):
-            if not node:
-                return False
-            if node.left or node.right:
-                return False
-            return True
 
-        ans = []
-
-        def helper(node, leaves):
+        arr = []
+        def recurse(node):
             if not node:
                 return 
-            if isleaf(node.left):
-                leaves.append(node.left.val)
-                node.left = None
-            else:
-                helper(node.left, leaves)
-            if isleaf(node.right):
-                leaves.append(node.right.val)
-                node.right = None
-            else:
-                helper(node.right, leaves)
-            return leaves
-
-        while root.left or root.right:
-            ans.append(helper(root, []))
-        
-        ans.append([root.val])
-        return ans
-        
-        
-        
-
-
-
-
+            if not node.left and not node.right:
+                arr[-1].append(node.val)
+                node = None
+                return None
             
-
+            if node.left:
+                node.left = recurse(node.left)
+            if node.right:
+                node.right = recurse(node.right)
+            return node
         
+        while root:
+            arr.append([])
+            root = recurse(root)
+        return arr
+
+

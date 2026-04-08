155b133d3cbdd3ae80d6326ba991037b6a0b7873
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2026-04-08 11:58:57 -0700

    Time: 13 ms (43.78%) | Memory: 19.3 MB (81.73%) - LeetSync

---
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        out = 0
+        for i in range(len(hours)-1):
+            for j in range(i+1, len(hours)):
+                if ((hours[i] + hours[j]) % 24 == 0):
+                    out += 1
+
+        return out

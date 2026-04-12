90f31a7eccf9c14388ab8b2fe8ac3e4a97e6eea7
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2026-04-12 14:18:58 -0700

    Time: 239 ms (30.30%) | Memory: 13.5 MB (55.23%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,18 +1,37 @@
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1
-        r = max(piles)
-        def caneat(num, new):
-            for p in piles:
-                new -= math.ceil(p/num)
-                if new < 0:
-                    return False
-            return True
-        
-        while l < r:
-            mid = (l + r )//2
-            if caneat(mid, h):
-                r = mid
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+
+        def canfinish(k):
+            hours = 0
+            for bananas in piles:
+                if bananas <= k:
+                    hours += 1
+                else:
+                    hours += (bananas//k) + (1 if bananas % k != 0 else 0)
+            if hours <= h:
+                return True
+            return False
+
+        if (h < len(piles)):
+            return False
+        low = 1
+        high = max(piles)
+        curmin = max(piles)
+        while low <= high:
+            midk = (low + high) // 2
+            if canfinish(midk):
+                curmin = min(curmin, midk)
+                high = midk - 1
             else:
-                l = mid + 1
-        return r
+                low = midk + 1
+            
+
+        return curmin
+
+
+        

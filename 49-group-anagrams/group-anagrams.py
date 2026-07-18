5fd5a373caee7e9d1f8e52cc6af32827f1eaae43
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2026-07-18 15:02:45 -0700

    Time: 56 ms (6.56%) | Memory: 16.6 MB (33.40%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -7,9 +7,11 @@ class Solution(object):
         """
         hs = defaultdict(list) #counter to list of words
         for word in strs:
-            sorted_word = str(sorted(word))
-            hs[sorted_word].append(word)
-        
-        return hs.values()
+            buckets = [0 for i in range(26)]
+            for letter in word:
+                buckets[ord('a')-ord(letter)] += 1
+            hs[str(buckets)].append(word)
+        return [v for v in hs.values()]
+
             
         
